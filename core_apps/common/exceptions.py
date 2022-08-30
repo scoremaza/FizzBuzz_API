c1c51fb386d3d86bb97832a1256d85391687e624
scoremaza <jconstantdevry@yahoo.com>
2022-08-30 18:30:36 -0400

    chore/feat:added folder structure/ timestamp model

---
+++ core_apps/common/exceptions.py
@@ -0,0 +1,52 @@
+from rest_framework.views import exception_handler
+
+
+def common_exception_handler(exc, context):
+    '''
+    If an error or exection occurs we what the django rest frame work not to handle
+    the exections but still get the response from django rest framework. 
+    '''
+    response = exception_handler(exc, context)
+
+    handlers = {
+        "NotFound": _handle_not_found_error,
+        "ValidationError": _handle_generic_error,
+    }
+
+    # This is how we will identify the type of exection that is thrown. 
+    exception_class = exc.__class__.__name__
+    
+    # Here we see if wee will handle it or allow Django Rest Framework to 
+    # handle the exception. 
+    if exception_class in handlers:
+        return handlers[exception_class](exc, context, response)
+    return response
+
+
+def _handle_generic_error(exc, context, response):
+    '''
+
+    '''
+    status_code = response.status_code
+    response.data = {"status_code": status_code, "errors": response.data}
+
+    return response
+
+
+def _handle_not_found_error(exc, context, response):
+    '''
+
+    '''
+    view = context.get("view", None)
+
+    if view and hasattr(view, "queryset") and view.queryset is not None:
+        status_code = response.status_code
+        error_key = view.queryset.model._meta.verbose_name
+        response.data = {
+            "status_code": status_code,
+            "errors": {error_key: response.data["detail"]},
+        }
+
+    else:
+        response = _handle_generic_error(exc, context, response)
+    return response
---
+++ core_apps/fizzbuzz_creator/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from core_apps.common.models import TimeStampedUUIDModel
+
